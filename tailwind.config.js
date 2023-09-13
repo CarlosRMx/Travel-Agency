69ab6d289318e6877d698bcb3ece5b5fac7b373a
Carlos Rodríguez <racm1698@gmail.com>
2023-09-12 18:36:58 -0600

    Se creo la carperta que contiene las imagenes del proyecto y tambien se inicio con la maquetacion de cada una de las secciones de manera general, ademas se modifico el archivo que configura tailwind para crear nuestro propio design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./public/index.html","./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'bali':"url('../img/bali.jpg')",
+        'chicago': "url('../img/chicago,jpg')",
+        'europe': "url('../img/europe.jpg')",
+        'iceland': "url('../img/iceland.jpg')",
+        'losAngeles': "url('../img/LA.jpg')",
+        'miami': "url('../img/miami.jpg')",
+        'newYork': "url('../img/new_york.jpg')",
+        'norway': "url('../img/norway.jpg')",
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+        'seattle': "url('../img/seattle.jpg')",
+        'switzerland': "url('../img/switzerland.jpg')",
+        'sydney': "url('../img/sydney.jpg')",
+        'yosemite': "url('../img/yosemite.jpg')", 
+      },
+      colors:{
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9',
+        'text-primary': '#000000',
+        'text-secondary': '#ffffff'
+      },
+      textColor:{
+        'primary':'#000000',
+        'secondary':'#ffffff',
+        'tertiary': '#CC2D4A',
+      },
+      fontFamily:{
+        Montserrat:['Montserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
